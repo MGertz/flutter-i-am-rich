a40f9bf8ffd4134d028cfa136ef768d969ecb765
Michael R. Gertz <michael@ringhus.dk>
2020-01-03 23:27:18 +0100

    added colors and backgrund image.

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,19 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-            title: Text("I Am Rich"),
-            backgroundColor: ,
+            title: Center(child: Text("I Am Rich")),
+            backgroundColor: Colors.blueGrey[900],
           ),
+
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://png2.cleanpng.com/sh/72a6764b96d539be13e2a77ad520b789/L0KzQYm3WcA1N5o0g9t8c4Dxd373jCJ1aZNxfZ93ZYT6f8PyTfdzaaFtgdV8LXPvecG0ggJ1NaN6eus2dILkfsT3ggJmdl4yTdQDZXTnQoeCVfM6QWMzTKk7OUK4SYm4VcM3OGkAS6o7NkG2RXB3jvc=/kisspng-portable-network-graphics-clip-art-ruby-transparen--5b8edd2695c992.4729259815360893826135.png'),
+          ),
+        ),
+
+
         ),
       ),
 
