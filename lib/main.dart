14d259c3abd242299ff77dcaef21f2ed9996d7cc
Michael R. Gertz <michael@ringhus.dk>
2020-01-04 09:24:34 +0100

    added app icons

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ void main() {
 
         body: Center(
           child: Image(
-            image: NetworkImage('https://png2.cleanpng.com/sh/72a6764b96d539be13e2a77ad520b789/L0KzQYm3WcA1N5o0g9t8c4Dxd373jCJ1aZNxfZ93ZYT6f8PyTfdzaaFtgdV8LXPvecG0ggJ1NaN6eus2dILkfsT3ggJmdl4yTdQDZXTnQoeCVfM6QWMzTKk7OUK4SYm4VcM3OGkAS6o7NkG2RXB3jvc=/kisspng-portable-network-graphics-clip-art-ruby-transparen--5b8edd2695c992.4729259815360893826135.png'),
+            image:
+              AssetImage('images/diamond.png')
+
           ),
         ),
 
